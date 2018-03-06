16290d1ed965ebf935bf5733a8b7f55199e5635b
PeterBusk <36533756+PeterBusk@users.noreply.github.com>
2018-03-06 11:42:05 +0100

    Create Book.java
    Book class, for use in other classes

---
+++ src/tribal_library/Book.java
@@ -0,0 +1,16 @@
+public class Book {
+
+private String title;
+private String author;
+private int year;
+private int ID;
+
+Book(String title, String author, String year, String publisher, int ID) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.ID = ID;
+    }
+
+
+}

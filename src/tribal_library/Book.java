5cf69c2d399c99c67b1dbf4ece1a90db56c750d5
PeterBusk <36533756+PeterBusk@users.noreply.github.com>
2018-03-06 11:44:54 +0100

    Update Book.java

--- src/tribal_library/Book.java
+++ src/tribal_library/Book.java
@@ -5,7 +5,7 @@ private String author;
 private int year;
 private int ID;
 
-Book(String title, String author, String year, String publisher, int ID) {
+Book(String title, String author, String year, int ID) {
         this.title = title;
         this.author = author;
         this.year = year;

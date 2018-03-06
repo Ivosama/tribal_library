80d950b8c9560effc88fec7032d627e24c9a931a
PolleAnker <thementoryay@gmail.com>
2018-03-06 12:52:14 +0100

    User class, finished?

--- src/tribal_library/User.java
+++ src/tribal_library/User.java
@@ -1,6 +1,42 @@
 package tribal_library;
 
-public abstract class User {
+public class User {
+	private int id;
 	private String password;
-	private String userID;
+	private String userName;
+	
+	User(){
+		
+	}
+	
+	User(int id, String password, String userName){
+		this.id = id;
+		this.password = password;
+		this.userName = userName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	private String getPassword() {
+		return password;
+	}
+
+	private void setPassword(String password) {
+		this.password = password;
+	}
+	
 }

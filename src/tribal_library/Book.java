8e3afb47051463cb9746a33ee2853995446cbca7
PeterBusk <36533756+PeterBusk@users.noreply.github.com>
2018-03-06 13:10:05 +0100

    Update Book.java
    Final Book class

--- src/tribal_library/Book.java
+++ src/tribal_library/Book.java
@@ -1,19 +1,58 @@
 public class Book {
 
-private String title;
-private String author;
-private int year;
-private int ID;
+	private int ID;
+	private String title;
+	private int year;
+	private int stock;
 
-Book() {
-    }        
-        
-Book(String title, String author, String year, int ID) {
-        this.title = title;
-        this.author = author;
-        this.year = year;
-        this.ID = ID;
-    }
+	Book() {
 
+	}        
+
+	Book(int ID, String title, int year, int stock) {
+		this.ID = ID;
+		this.title = title;
+		this.year = year;
+		this.stock = stock;
+	}
+
+	public int getID() {
+		return this.ID;
+	}
+	public void setID(int ID) {
+		this.ID = ID;
+	}
+
+	public String getTitle() {
+		return this.title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getYear() {
+		return this.year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public int getStock() {
+		return this.stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	public boolean isInStock() {
+		if (this.stock > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void displayBookDetails() {
+		System.out.println("ID: " + this.ID + ", Title: " + this.title + ", Year: " + this.year + ", Current stock: " + this.stock);
+	}
 
 }

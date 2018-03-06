83a923a2b9d90e52fd2d546de5675d5545baee40
PeterBusk <36533756+PeterBusk@users.noreply.github.com>
2018-03-06 11:47:23 +0100

    Update ManageBooks.java

--- src/tribal_library/ManageBooks.java
+++ src/tribal_library/ManageBooks.java
@@ -1,9 +1,15 @@
 public class ManageBooks {
+    
+    static int maxBooks = 1000;
+    int bookCounter = 0;
+    
+    Book[] books = new Book[maxBooks];
 
-void addBook(String title, String author) {
-
+void addBook(String title, String author, int year) {
+        books[bookCounter] = new book(title, author, year, bookCounter);
+        bookCounter++;
     }
 void removeBook(int ID) {
-
+        
     }
 }

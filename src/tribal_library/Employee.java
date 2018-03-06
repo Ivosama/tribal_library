d3f6ee1e81cc11b3d20475ca133e39bfab86787b
ivand <ivandts@gmail.com>
2018-03-06 12:58:15 +0100

    Employee finished

--- src/tribal_library/Employee.java
+++ src/tribal_library/Employee.java
@@ -4,8 +4,29 @@ public class Employee extends User {
 	private String name;
 	private String type;
 	
+	Employee (){
+		super();
+	}
+	
 	Employee(String name, String type){ // Default constructor
 		this.name = name;
 		this.type = type;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
 }

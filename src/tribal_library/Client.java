cdf80e5a6dc48efdf22df4346e48253fb18d3ffe
Zephan <Zephan@DESKTOP-GL2H4LI>
2018-03-06 13:32:16 +0100

    put things in Client

--- src/tribal_library/Client.java
+++ src/tribal_library/Client.java
@@ -1,5 +1,44 @@
 package tribal_library;
 
-public class Client {
+public class Client extends User{
+	private String name;
+	private String address;
+	private String phoneNo;
+	
+	Client(){
+	}
+	
+	Client(String name, String address, String phoneNo){
+		this.name = name;
+		this.address = address;
+		this.phoneNo = phoneNo;
+	}
+	
+	public String getName() {
+		return name;
+	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+	
+	
+	
 }

4b53d0e412d177e5d68ed7168e4ba39c49984150
MSI <swiftrow@gmail.com>
2018-03-08 10:43:38 +0100

    Da rest of da code

---
+++ src/tribal_library/Borrow.java
@@ -0,0 +1,57 @@
+package tribal_library;
+import java.util.Date;
+
+
+
+
+public class Borrow {
+
+	public Book book;
+	public Date startDate;
+	public Date endDate;
+	public Notification notification;
+	
+	public Book searchBook() {
+		return book;
+				
+	}
+	public void borrowWindow() {
+		
+	}
+	public Book borrowBook() {
+		return book;
+	}
+	public void addBook() {
+		
+	}
+	public void removeBook() {
+		
+	}
+	public void viewManageWindow() {
+		
+	}
+	public Book getBook() {
+		return book;
+	}
+	public void setBook(Book book) {
+		this.book = book;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	public Notification getNotification() {
+		return notification;
+	}
+	public void setNotification(Notification notification) {
+		this.notification = notification;
+	}
+}
--- src/tribal_library/Notification.java
+++ src/tribal_library/Notification.java
@@ -1,8 +1,25 @@
 package tribal_library;
+import java.util.Date;
 
 public class Notification {
-	private String noBook;
+	public Date dateSent;
 	
+	public void setNotificationDelay() {
+		
+	}
+	
+	public void returnMessage() {
+		
+	}
+	
+	public Date getDateSent() {
+		return dateSent;
+	}
+
+	public void setDateSent(Date dateSent) {
+		this.dateSent = dateSent;
+	}
+
 	Notification(){
 		
 	}
